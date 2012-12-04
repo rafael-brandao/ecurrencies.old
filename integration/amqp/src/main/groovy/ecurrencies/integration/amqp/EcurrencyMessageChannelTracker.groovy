1d2ef804c25941a310d44c4b1d5d096ff99403e8
Rafael de Andrade <rafa.bra@gmail.com>
2012-12-04 11:54:27 -0200

    Removed code duplication.
    Moved duplicated code to a new method that allows a closure to be
    executed.

--- integration/amqp/src/main/groovy/ecurrencies/integration/amqp/EcurrencyMessageChannelTracker.groovy
+++ integration/amqp/src/main/groovy/ecurrencies/integration/amqp/EcurrencyMessageChannelTracker.groovy
@@ -1,8 +1,7 @@
-package ecurrencies.integration.amqpimport static ecurrencies.commons.util.PropertyResolver.getRequiredPropertyimport static java.lang.String.formatimport java.util.concurrent.ConcurrentHashMapimport org.springframework.beans.factory.annotation.Autowiredimport org.springframework.context.ApplicationContextimport org.springframework.integration.endpoint.AbstractEndpointimport org.springframework.scheduling.annotation.Async
+package ecurrencies.integration.amqpimport static ecurrencies.commons.util.PropertyResolver.getRequiredPropertyimport static java.lang.String.formatimport java.util.concurrent.ConcurrentHashMapimport org.springframework.beans.factory.annotation.Autowiredimport org.springframework.context.ApplicationContextimport org.springframework.scheduling.annotation.Async
 class EcurrencyMessageChannelTracker  {    private static final String ECURRENCY_PROVIDER = 'ecurrency-provider'
     private static final String ECURRENCY_SERVICE = 'ecurrency-service'
     private static final String BEAN_NAME = 'amqpInboundChannelAdapter'
-    private static final String ID_FORMAT = '%s-%s'
     // <Long, AbstractEndpoint>
     private final Map endpointMap = [:] as ConcurrentHashMap
 
@@ -13,18 +12,6 @@ class EcurrencyMessageChannelTracker  {    private static final String ECURREN
         super()    }
 
     @Async
-    void addedChannel(messageChannel, properties) {
-        def ep = getRequiredProperty(ECURRENCY_PROVIDER, properties)
-        def es = getRequiredProperty(ECURRENCY_SERVICE, properties)
+    void addedChannel(messageChannel, properties) {        channelAction(messageChannel, properties) { String ep, es ->            endpointMap << [(id(ep, es)):(applicationContext.getBean(BEAN_NAME, messageChannel, ep, es))]        }    }
 
-        def endpoint = (AbstractEndpoint) applicationContext.getBean(BEAN_NAME, messageChannel, ep, es)
-        endpointMap << [(id(ep, es)):endpoint]    }
-
-    public void removingChannel(messageChannel, properties) {
-        def ep = getRequiredProperty(ECURRENCY_PROVIDER, properties)
-        def es = getRequiredProperty(ECURRENCY_SERVICE, properties)
-
-        AbstractEndpoint endpoint
-
-        if ((endpoint = endpointMap.remove((id(ep, es)))) {
-            endpoint.stop()        }    }    private static def id(String[] args) {        def hash = 0L        args.each {            hash += it.hashCode()        }        hash    }}
+    public void removingChannel(messageChannel, properties) {        channelAction(messageChannel, properties) { String ep, es ->            endpointMap.remove(id(ep, es))?.stop()        }    }    private void channelAction(messageChannel, properties, closure) {        def ep = getRequiredProperty(ECURRENCY_PROVIDER, properties)        def es = getRequiredProperty(ECURRENCY_SERVICE, properties)        closure(ep, es)    }    private static def id(String[] args) {        def hash = 0L        args.each { hash += it.hashCode() }        hash    }}
